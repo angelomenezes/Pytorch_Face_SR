c70de0028b15bd631043b97e50414e170c867631
angelomenezes <angelomenezes.eng@gmail.com>
2019-11-11 18:28:38 -0300

    last changes to scripts

--- dataset/super_resolution_data_loader_resize.py
+++ dataset/super_resolution_data_loader_resize.py
@@ -22,9 +22,9 @@ def load_image(path):
     y, _, _ = img.split()
     return y  
 
-#def totensor():
-#    return transforms.Compose([transforms.ToTensor(),])
-                               #transforms.Normalize(0.5, 0.5),])
+def totensor():
+    return transforms.Compose([transforms.ToTensor(),])
+                               #transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5)),])
 
 class DatasetSuperRes(Dataset):
     def __init__(self, image_dir, target_dir, transform=None):        
@@ -35,17 +35,17 @@ class DatasetSuperRes(Dataset):
         img_input = np.array(load_image(self.image_filenames[index]))
         target = np.array(load_image(self.target_filenames[index]))
        
-        img_input = torch.from_numpy(img_input).float()
-        target = torch.from_numpy(target).float()
+        #img_input = torch.from_numpy(img_input).float()
+        #target = torch.from_numpy(target).float()
         
-        img_input = img_input.view(1,img_input.shape[0],img_input.shape[1])
-        target = target.view(1,target.shape[0],target.shape[1])
+        #img_input = img_input.view(1,img_input.shape[0],img_input.shape[1])
+        #target = target.view(1,target.shape[0],target.shape[1])
         
-        img_input = img_input / 255.0
-        target = target / 255.0
+        #img_input = img_input / 255.0
+        #target = target / 255.0
         
-        #img_input = totensor()(img_input)
-        #target = totensor()(target)
+        img_input = totensor()(img_input)
+        target = totensor()(target)
         
         return img_input, target
 
--- loss.py
+++ loss.py
@@ -1,7 +1,94 @@
 import torch
 from torch import nn
 from torchvision.models.vgg import vgg16
+from numpy.linalg import norm
+import numpy as np
+model_path = '/home/angelo/Desktop/Github/Feature-Extractors/'
+os.sys.path.append(model_path)
+import senet50_128_pytorch.senet50_128 as model
+    
+def initialize_senet50():
+    network = model.senet50_128(weights_path=model_path + 'senet50_128_pytorch/senet50_128.pth')
+    network.eval()
+    return network
 
+def cos_sim(a,b):
+    return dot(a, b)/(norm(a)*norm(b))
+
+def euc_dis(a,b):
+    return norm(a-b)
+
+def identity_loss(model, img1, img2):
+    img1_emb = return_embedding(model, img1)
+    img2_emb = return_embedding(model, img2)
+    return 1 - cos_sim(img1_emb, img2_emb)
+
+def return_embedding(model, image):
+    face_feats = np.empty((1,128)) # Embedding size
+    im_array = np.array(image).reshape((1,3,224,224))
+    f = model(torch.Tensor(im_array))[1].detach().cpu().numpy()[:, :, 0, 0]
+    face_feats = f / np.sqrt(np.sum(f ** 2, -1, keepdims=True)) # Normalizing embedding
+    return face_feats.reshape(-1)
+
+def return_activations(model, image):
+    activations = list()
+    normalized_values = 0
+    im_array = np.array(image).reshape((1,3,224,224))
+    for i in range(2,7):
+        f = model(torch.Tensor(im_array))[i].detach().cpu().numpy()[:, :, 0, 0]
+        normalized_values = f / np.sqrt(np.sum(f ** 2, -1, keepdims=True))
+        activations.append(normalized_values.reshape(-1))
+    return np.array(activations)
+
+def face_content_loss(model, img1, img2, metric='L2'):
+    # Specific for SeNet50
+    activations_img1 = return_activations(model, img1)
+    activations_img2 = return_activations(model, img2)
+    loss = 0
+    
+    # L1 Error or L2 Error or Euclidean Distance
+    for layer in range(len(activations_img1)):
+        if metric == 'L1':
+            loss += np.sum(np.abs(activations_img1[layer] - activations_img2[layer]))
+        elif metric == 'L2':
+            loss += np.sum((activations_img1[layer] - activations_img2[layer])**2)
+        else:
+            loss += euc_dis(activations_img1[layer], activations_img2[layer])
+    return lossclass FacePerceptionLoss(nn.Module):
+    def __init__(self):
+        super(FacePerceptualLoss, self).__init__()
+        self.mse_loss = nn.MSELoss()
+        self.tv_loss = TVLoss()
+        self.model = initialize_senet50()
+
+    def forward(self, out_images, target_images):
+        
+        # Identity Loss
+        identity_loss = identity_loss(self.model, out_images, target_images)
+        # Content Loss
+        content_loss = face_content_loss(self.model, out_images, target_images)
+        # Image Loss
+        image_loss = self.mse_loss(out_images, target_images)
+        
+        return image_loss +  0.1 * identity_loss + 0.1 * content_loss
+
+class FacePerceptionLoss(nn.Module):
+    def __init__(self):
+        super(FacePerceptualLoss, self).__init__()
+        self.mse_loss = nn.MSELoss()
+        self.tv_loss = TVLoss()
+        self.model = initialize_senet50()
+
+    def forward(self, out_images, target_images):
+        
+        # Identity Loss
+        identity_loss = identity_loss(self.model, out_images, target_images)
+        # Content Loss
+        content_loss = face_content_loss(self.model, out_images, target_images)
+        # Image Loss
+        image_loss = self.mse_loss(out_images, target_images)
+        
+        return image_loss +  0.1 * identity_loss + 0.1 * content_loss
 
 class GeneratorLoss(nn.Module):
     def __init__(self):
@@ -44,6 +131,7 @@ class TVLoss(nn.Module):
     @staticmethod
     def tensor_size(t):
         return t.size()[1] * t.size()[2] * t.size()[3]
+
 
 
 if __name__ == "__main__":
