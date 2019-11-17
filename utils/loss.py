df370dd92247491595c4d2f9e794ca6b84433595
angelomenezes <angelomenezes.eng@gmail.com>
2019-11-16 22:19:03 -0300

    changed face loss to identity loss with inception resnet

--- utils/loss.py
+++ utils/loss.py
@@ -8,114 +8,32 @@ from numpy import dot
 from numpy.linalg import norm
 import os
 
-#model_path = '/home/angelo/Desktop/Github/Feature-Extractors/'
-model_path = '/home/jupyter/Feature-Extractors/'
-
-os.sys.path.append(model_path)
-
-def initialize_senet50_2048():
-    import senet50_ft_pytorch.senet50_ft_dims_2048 as model
-    network = model.senet50_ft(weights_path=model_path + 'senet50_ft_pytorch/senet50_ft_dims_2048.pth')
-    network.eval()
-    return network
-
 def cos_sim(a,b):
     return 1 - dot(a, b)/(norm(a)*norm(b))
 
 def euc_dis(a,b):
     return norm(a-b)
 
-def return_activations(model, image):
-    '''
-    Currently it only accepts SeNet50 activations.
-    '''
-    activations = list()
-    normalized_values = 0
-    for i in range(2,7):
-        f = model(image.view(1,3,image.shape[1],image.shape[2]))[i].detach().cpu().numpy()[:, :, 0, 0]
-        normalized_values = f / np.sqrt(np.sum(f ** 2, -1, keepdims=True))
-        activations.append(normalized_values.reshape(-1))
-    return np.array(activations)
-
-def face_content_loss(model, img1, img2, metric='L2'):
-    
-    img1 = VGG2Face_normalization(img1)
-    img2 = VGG2Face_normalization(img2)
-    
-    # Specific for SeNet50
-    activations_img1 = return_activations(model, img1)
-    activations_img2 = return_activations(model, img2)
-    loss = 0
-    
-    # L1 Error or L2 Error or Euclidean Distance
-    for layer in range(len(activations_img1)):
-        if metric == 'L1':
-            loss += np.sum(np.abs(activations_img1[layer] - activations_img2[layer]))
-        elif metric == 'L2':
-            loss += np.sum((activations_img1[layer] - activations_img2[layer])**2)
-        else:
-            loss += euc_dis(activations_img1[layer], activations_img2[layer])
-    return loss
-
-def batch_face_content_loss(model, batch_img1, batch_img2):
-    batch_size = batch_img1.shape[0]
-    loss = 0
-    for index in range(batch_size):
-        loss += face_content_loss(model, batch_img1[index],batch_img2[index])
-    
-    return loss/batch_size
-
-def return_embedding(model, image):
-    f = model(image.view(1,3,image.shape[1],image.shape[2]))[1].detach().cpu().numpy()[:, :, 0, 0]
-    face_feats = f / np.sqrt(np.sum(f ** 2, -1, keepdims=True)) # Normalizing embedding
-    return face_feats.reshape(-1)
-
-def face_identity_loss(model, img1, img2):
-    img1 = VGG2Face_normalization(img1)
-    img2 = VGG2Face_normalization(img2)
-    
-    img1_emb = return_embedding(model, img1)
-    img2_emb = return_embedding(model, img2)
-    return cos_sim(img1_emb, img2_emb)
-
-def batch_face_identity_loss(model, batch_img1, batch_img2):
-    batch_size = batch_img1.shape[0]
-    loss = 0
-    for index in range(batch_size):
-        loss += face_identity_loss(model, batch_img1[index], batch_img2[index])
-    
-    return loss/batch_size
-
-def VGG2Face_normalization(img):
-    '''
-    Normalization according to parameters of the used architecture
-    '''
-    VGG_mean = (131.0912, 103.8827, 91.4953)
-    VGG_std = (1, 1, 1)
-    return transforms.Normalize(VGG_mean,VGG_std)(img)
-
-class FacePerceptionLoss(nn.Module):
+class FaceIdentityLoss(nn.Module):
     def __init__(self, model):
-        super(FacePerceptionLoss, self).__init__()
-        self.mse_loss = nn.MSELoss()
-        self.tv_loss = TVLoss()
+        super(FaceIdentityLoss, self).__init__()
         self.model = model
-        
+
+    def face_identity_loss(self, img1, img2): 
+        img1_emb = self.model(img1.unsqueeze(0)).squeeze(0).detach().cpu().numpy()
+        img2_emb = self.model(img2.unsqueeze(0)).squeeze(0).detach().cpu().numpy()
+        return cos_sim(img1_emb, img2_emb)
+
+    def batch_face_identity_loss(self, batch_img1, batch_img2):
+        batch_size = batch_img1.shape[0]
+        loss = 0
+        for index in range(batch_size):
+            loss += self.face_identity_loss(batch_img1[index], batch_img2[index])
+        return loss/batch_size
 
     def forward(self, out_images, target_images):
-        
-        # Scaling back the data from (0,1) to (0,255) since SEResNet did not use such standard
-        out_images *= 255
-        target_images *= 255
-        
-        # Identity Loss
-        identity_loss = batch_face_identity_loss(self.model, out_images, target_images)
-        # Content Loss
-        content_loss = batch_face_content_loss(self.model, out_images, target_images)
-        # Image Loss
-        image_loss = self.mse_loss(out_images, target_images)
-        
-        return image_loss + 0.01 * identity_loss + 0.01 * content_loss
+        identity_loss = self.batch_face_identity_loss(out_images, target_images) 
+        return identity_loss
 
 class GeneratorLoss(nn.Module):
     def __init__(self):
